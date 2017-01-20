6e5566534dd868d3a08886a9e8bfb938130759fc
R Wood <archaurore@gmail.com>
2017-01-19 20:21:06 -0500

    added click and change events on form in list component

--- js/components/list.js
+++ js/components/list.js
@@ -3,21 +3,54 @@ import React from 'react';
 import Card from './card';
 
 export default function List(props) {
-	const cards = [];
+	// const cards = [];
 
-	for (let i = 0; i < 3; i++) {
-		cards.push(<Card />);
-	}
+	// for (let i = 0; i < 3; i++) {
+	// 	cards.push(<Card />);
+	// }
 
 	return (
 		<div className='list'>
 			<h3 className="list-title">{props.title}</h3>
-			{cards}
 			<form>
 				<label for="text-input">Text Input:</label>
-				<input id="text-input" type="text" />
+				<input id="text-input" type="text" onChange={props.onChange} />
+				<button type="submit" onSubmit={props.onSubmit}>Submit</button>
 			</form>
 		</div>
 	);
 }
 
+class Change extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			changed: false
+		}
+
+		this.onAddInputChange = this.onAddInputChange.bind(this);
+	}
+
+	onAddInputChange() {
+		console.log('change event triggered');
+		this.setState({
+			changed: true
+		});
+	}
+}
+
+class Submit extends React.Component {
+	constructor(props) {
+		super(props);
+
+		e.preventDefault();
+
+		this.onAddSubmit = this.onAddSubmit.bind(this);
+	}
+
+	onAddSubmit() {
+		console.log('submit event triggered');
+
+	}
+}
