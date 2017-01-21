6cec91373b171667b7bdfb876a7529e249fa8975
R Wood <archaurore@gmail.com>
2017-01-20 19:31:53 -0500

    almost got it running

--- js/components/list-container.js
+++ js/components/list-container.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import List from './list';
 
-export class ListContainer extends React.Component {
+export default class ListContainer extends React.Component {
 	constructor(props) {
 		super(props);
 
@@ -13,6 +13,20 @@ export class ListContainer extends React.Component {
 		this.onAddInputChange = this.onAddInputChange.bind(this);
 		this.onAddSubmit = this.onAddSubmit.bind(this);
 	}
+
+	onAddInputChange(event) {
+		console.log('change event triggered');
+		this.setState({text: event.target.value});
+	}
+
+	onAddSubmit(e) {
+		e.preventDefault();
+		console.log('submit event triggered');
+		let newCards = [];
+		newCards.push(this.props.text);
+		this.setState({cards: newCards});
+	}
+
 
 	render() {
 		return <List cards={this.state.cards} onChange={this.state.onAddInputChange} onSubmit={this.state.onAddSubmit} />;
--- js/components/list.js
+++ js/components/list.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
-import Card from './card';
-
-export default function List(props) {
-
+// import Card from './card';
+export class List extends ListContainer {
+// export default function List(props) {
+	// return <div>puppes</div>;
 	return (
 		<div className='list'>
 			<h3 className="list-title">{props.title}</h3>
 			<form>
-				<label for="text-input">Text Input:</label>
+				<label htmlFor="text-input">Text Input:</label>
 				<input id="text-input" type="text" onChange={props.onChange} />
 				<button type="submit" onSubmit={props.onSubmit}>Submit</button>
 			</form>
@@ -16,31 +16,31 @@ export default function List(props) {
 	);
 }
 
-class Change extends React.Component {
-	constructor(props) {
-		super(props);
+// class Change extends React.Component {
+// 	constructor(props) {
+// 		super(props);
 
-		this.onAddInputChange = this.onAddInputChange.bind(this);
-	}
+// 		this.onAddInputChange = this.onAddInputChange.bind(this);
+// 	}
 
-	onAddInputChange(event) {
-		console.log('change event triggered');
-		this.setState({text: event.target.value});
-	}
-}
+// 	onAddInputChange(event) {
+// 		console.log('change event triggered');
+// 		this.setState({text: event.target.value});
+// 	}
+// }
 
-class Submit extends React.Component {
-	constructor(props) {
-		super(props);
+// class Submit extends React.Component {
+// 	constructor(props) {
+// 		super(props);
 
-		this.onAddSubmit = this.onAddSubmit.bind(this);
-	}
+// 		this.onAddSubmit = this.onAddSubmit.bind(this);
+// 	}
 
-	onAddSubmit(e) {
-		e.preventDefault();
-		console.log('submit event triggered');
-		let newCards = [];
-		newCards.push(this.props.text);
-		this.setState({cards: newCards});
-	}
-}
+// 	onAddSubmit(e) {
+// 		e.preventDefault();
+// 		console.log('submit event triggered');
+// 		let newCards = [];
+// 		newCards.push(this.props.text);
+// 		this.setState({cards: newCards});
+// 	}
+// }
