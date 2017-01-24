b242b2d224292fe5ba40b1c1d517eb9c6c14f01e
R Wood <archaurore@gmail.com>
2017-01-24 08:35:56 -0500

    added way to render card properly on update

--- js/components/card.js
+++ js/components/card.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default function Card(props) {
-	return (
-		<div className="card">
-			<p className="card-content">{props.content}</p>
-		</div>
-	);
+    console.log('props',props);
+    return (
+        <div className="card">
+            {props['cardContent'].map(function(cContent, index){
+                  return <p className="card-content" key={ index }>{cContent}</p>;
+      })}
+        </div>
+    );
 }
