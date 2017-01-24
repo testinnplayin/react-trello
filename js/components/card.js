b65d00dba071ebd8c0a384be89fba924d1ff1459
Jack Melnick <mlnck@Jacks-Mac-Pro.local>
2017-01-23 19:10:49 -0500

    Cards Working

--- js/components/card.js
+++ js/components/card.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 export default function Card(props) {
+	console.log('props',props);
 	return (
 		<div className="card">
-			<p className="card-content">{props.content}</p>
+			{
+				props['cardContent'].map(function(cContent, index)
+				{ return <p className="card-content" key={ index }>{cContent}</p>; })
+			}
 		</div>
 	);
 }
